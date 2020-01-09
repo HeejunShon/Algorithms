32758895db1bae533bcbb2600febabde3ebe4415
HeejunShon <53172999+HeejunShon@users.noreply.github.com>
2020-01-09 20:02:59 +0900

    change some name

--- SE_2050.java
+++ SE_2050.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class SE2050 {
+public class SE_2050 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- SE_2063.java
+++ SE_2063.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Solution {
+public class SE_2063{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
