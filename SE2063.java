aa7c45b45954019b787cba35bd23315755692617
HeejunShon <53172999+HeejunShon@users.noreply.github.com>
2020-01-09 19:51:38 +0900

    SE/2063/solved

---
+++ SE2063.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		int[] arr = new int[T];
+		
+		int answer = 0;
+		for(int tc = 1; tc<= T; tc++){
+			arr[tc - 1] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		//중간값 찾기
+		System.out.println(arr[(T-1)/2]);
+	}
+
+}
