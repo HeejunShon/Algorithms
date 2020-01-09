9822dc2fbc8e0c641c3022d5aab1fe870263408f
HeejunShon <53172999+HeejunShon@users.noreply.github.com>
2020-01-09 19:51:21 +0900

    SE/2050/solved

---
+++ SE2050.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SE2050 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		Scanner sc = new Scanner(System.in);
+		
+		String line = br.readLine();
+//		String line = sc.next();
+		
+		//알파벳을 숫자로 변환
+		for(int i = 0; i<line.length(); i++){
+			System.out.print(line.charAt(i)-'A' + 1 +" ");
+		}
+	}
+}
